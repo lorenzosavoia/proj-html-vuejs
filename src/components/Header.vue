db3a187e29eb9f0b057d25e73afd6a5673fbad7b
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-01-24 11:41:49 +0100

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <header>
+      <div class="contacts container">
+          <div class="row">
+                <div class="col 7">
+                    <div class="row">
+                        <div class="col-3">
+                            Opern to 313124
+                        </div>
+                        <div class="col-4">
+                            ciao
+                        </div>
+                    </div>
+                </div>
+          </div>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name:'Header', 
+}
+</script>
+
+<style lang='scss' scoped>
+ @import "~bootstrap/scss/bootstrap";
+ 
+ .contacts{
+     background-color: #21333e;
+     width: 1590px;
+ }
+
+</style>
