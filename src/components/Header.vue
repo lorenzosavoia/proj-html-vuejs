00e3aaa53cf7569ecba88126525f79ef37acf7fd
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-01-26 14:03:19 +0100

    add header componets

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,97 +1,21 @@
 
 <template>
   <div class="container-fluid">
-    <!-- contacts -->
-      <div class="row justify-content-center contacts align-items-center">
-          <div class="col-8">
-              <div class="row justify-content-between">
-                    <div class="col-4 ">
-                        <div class="text"><i class="fas fa-clock me-2"></i>Open Hours Mon-Sat - 9.00 - 18-00</div>
-                    </div>  
-                    <div class="col 4 d-flex justify-content-end">
-                          <div class="phone me-3">
-                              <i class="fas fa-phone-alt me-2"></i>
-                              <span>+1 (305) 1234-56789</span>
-                          </div>
-                          <div class="e-mail me-3">
-                              <i class="fas fa-envelope me-2"></i>
-                              <span>hello@example.com</span>
-                          </div>
-                          <div class="social me-3">
-                              <i class="fab fa-facebook-f me-3"></i>
-                              <i class="fab fa-twitter me-3"></i>
-                              <i class="fab fa-linkedin-in me-3"></i>
-                          </div>
-                    </div>
-              </div>
-          </div>
-      </div>
-    <!-- contacts -->
-    <div class="jumbotron row pt-3">
-        <!-- <img src="..\assets\bg-6.jpg" alt=""> -->
-        <div class="row nav-bar row justify-content-center align-items-center">
-            <!-- navbar -->
-            <div class="col-8 d-flex justify-content-between">
-                <div class="next-gen-logo"> <h1>NEXTGEN</h1></div>
-                <div class="nav-contacts d-flex align-items-center">
-                    <ul class="d-flex">
-                        <li><a href="">HOME</a></li>
-                        <li><a href="">ABOUT</a> </li>
-                        <li><a href="">SERVICES</a> </li>
-                        <li><a href="">PROCESS</a> </li>
-                        <li><a href="">TEAM</a> </li>
-                        <li><a href="">BLOG</a> </li>
-                    </ul>
-                    <button class="btn btn-primary" type="submit">GET IN TOUCH</button>
-                </div>
-                <!-- navbar -->
-            </div>
-        </div>
-        <!-- form -->
-        <div class="row justify-content-center jumbo-form">
-            <div class="col-8">
-                <div class="row justify-content-end">
-                    <div class="col-5">
-                        <div class="row">
-                            <div class="col-12 mb-3">
-                                <div class="fusion-text">FUSION & ACQUISTION</div>
-                                <h1 class="title-form">Insurnce</h1>
-                                <h1 class="title-form">Consulting</h1>
-                            </div>
-                            <div class="col-6 d-flex align-items-center">
-                                <input type="text" class="form" id="" placeholder="Name">
-                            </div>
-                            <div class="col-6 d-flex align-items-center">
-                                <input type="email" class="form" id="" placeholder="Email">
-                            </div>
-                            <div class="col-6 d-flex align-items-center mt-2">    
-                                <input type="text" class="form" id="" placeholder="phone">
-                            </div>
-                            <div class="col-6 d-flex align-items-center mt-2">
-                                <select class="select" aria-label="Default select example">
-                                    <option selected>More info</option>
-                                    <option value="1">Azienda</option>
-                                    <option value="2">Libero professionista</option>
-                                    <option value="3">Privato</option>
-                                </select>
-                            </div>
-                            <div class="col-12 mt-2">
-                                <button class="btn btn-primary me-3" type="submit">GET IN TOUCH</button>
-                                <button type="button" class="btn btn-outline-success">READ MORE</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>   
-            </div>
-        </div>
-        <!-- form -->
-    </div>
+    <Navbar></Navbar>
+    <Jumbotron></Jumbotron>
+    
   </div>
 </template>
 
 <script>
+import Navbar from '../components/headerComponents/Navbar.vue'
+import Jumbotron from '../components/headerComponents/Jumbotron.vue'
 export default {
   name:'Header',
+  components: {
+      Navbar,
+      Jumbotron,
+  },
   data(){
       return{
         //   image:"../assets/bg-6.png"
@@ -105,92 +29,8 @@ export default {
  @import "~bootstrap/scss/bootstrap";
  
  .container-fluid{
-     .contacts{
-         height: 45px;
-        color: #bfc0ca;
-        background-color: #21333e;
-        font-size: .8em;
-    }
-    .jumbotron{
-        background-image: url('~@/assets/bg-6.jpg');
-        height: 700px;
-        .btn-primary{
-            background-color:#058183 ;
-            border-color:#058183; 
-            color: white;
-            display: inline-block;
-        }
-        .btn-outline-success{
-        color:white;
-        background-color: #058183;
-        border-color: #058183;
-        }
-
-       .nav-bar{
-           height: 40px;
-            
-            ul{
-                list-style: none;
-                margin: 0;
-                padding: 0;
-                li{
-                    display: flex;
-                    align-items: center;
-                    a{
-                        text-decoration: none;
-                        margin-right: 1em;
-                        display: inline-block;
-                        color: black;
-                    } 
-                }       
-            }
-       }
-       .jumbo-form{
-           .form{
-                display: block;
-                width: 100%;
-                padding: 0.375rem 0.75rem;
-                font-size: 1rem;
-                font-weight: 400;
-                line-height: 1.5;
-                color: #939aa3;
-                background-color: #ccd1d4;
-                background-clip: padding-box;
-                border: 1px solid #ccd1d4;
-                appearance: none;
-                border-radius: 0.25rem;
-                transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-           }
-           .select{
-                display: block;
-                width: 100%;
-                padding: 0.375rem 2.25rem 0.375rem 0.75rem;
-                -moz-padding-start: calc(0.75rem - 3px);
-                font-size: 1rem;
-                font-weight: 400;
-                line-height: 1.5;
-                color: #939aa3;
-                background-color: #ccd1d4;
-                background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='%23343a40' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M2 5l6 6 6-6'/%3e%3c/svg%3e");
-                background-repeat: no-repeat;
-                background-position: right 0.75rem center;
-                background-size: 16px 12px;
-                border: 1px solid #ccd1d4;
-                border-radius: 0.25rem;
-                transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-                appearance: none;
-           }
-           .fusion-text{
-               color:#4abac5;
-               font-weight: 600;
-           }
-           .title-form{
-               color: #21333e;
-               font-size: 4em;
-               font-weight: 800;
-           }
-       }
-    }
+     
+    
  }
 
 </style>
---
+++ src/components/headerComponents/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <!-- contacts -->
+      <div class="row justify-content-center contacts align-items-center">
+          <div class="col-8">
+              <div class="row justify-content-between">
+                    <div class="col-4 ">
+                        <div class="text"><i class="fas fa-clock me-2"></i>Open Hours Mon-Sat - 9.00 - 18-00</div>
+                    </div>  
+                    <div class="col 4 d-flex justify-content-end">
+                          <div class="phone me-3">
+                              <i class="fas fa-phone-alt me-2"></i>
+                              <span>+1 (305) 1234-56789</span>
+                          </div>
+                          <div class="e-mail me-3">
+                              <i class="fas fa-envelope me-2"></i>
+                              <span>hello@example.com</span>
+                          </div>
+                          <div class="social me-3">
+                              <i class="fab fa-facebook-f me-3"></i>
+                              <i class="fab fa-twitter me-3"></i>
+                              <i class="fab fa-linkedin-in me-3"></i>
+                          </div>
+                    </div>
+              </div>
+          </div>
+      </div>
+    <!-- contacts -->
+
+</template>
+
+<script>
+export default {
+    name:'Navbar'
+}
+</script>
+
+<style lang="scss">
+    .contacts{
+        height: 45px;
+        color: #bfc0ca;
+        background-color: #21333e;
+        font-size: .8em;
+    }
+</style>
