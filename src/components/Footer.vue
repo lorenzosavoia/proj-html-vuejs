4a0b146ab2408e718b824f4c108a2ebd88e9136b
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-01-26 16:25:09 +0100

    fixing footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,9 @@
   <div class="row main-footer justify-content-center align-items-center">
     <div class="col-8 d-flex justify-content-between align-items-center">
       <div class="col-2 flex-fill">
-        <h2>NEXGEN</h2>
+        <div class="nex-gen-logo"> 
+          <h1><span class="">NEX</span>GEN</h1>
+        </div>
         <span>A functional HTML template for corporate & business</span>.
         <div class="phone me-3 mb-2 mt-2">
           <i class="fas fa-phone-alt me-2"></i>
@@ -40,7 +42,7 @@
         <span>Enjoy the low price. We are traking any inention of piracy.</span>
       </div>
       <div class="col-4">
-        <span><i class="far fa-copyright"></i>2020 NEXGEN is Proudly Powered by <span>Codings</span></span>
+        <span><i class="far fa-copyright"></i>2020 NEXGEN is Proudly Powered by <span class="green">Codings</span></span>
       </div>
     </div>
 
@@ -96,7 +98,26 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.nex-gen-logo{     
+  h1{
+      color: #b8b7bb;
+      font-size: 1.3em;
+      font-weight: 700;
+  }
+  span{
+      display: inline-block;
+      background-color: #0e272d;
+      color: #01a6a6;
+      border-top-left-radius:30px;
+      border-bottom-left-radius: 30px;
+      padding:.4em .2em .4em 1.2em;
+      margin-right: .2em;
+  }
+}
+.green{
+  color: #0a5863;
+}
 .second-footer{
   height: 75px;
   background-color: #0d0d11;
@@ -104,6 +125,9 @@ export default {
 }
 ul{
   list-style: none;
+  li{
+    color: #b8b7bb;
+  }
 
 }
 .main-footer{
@@ -120,12 +144,12 @@ ul{
 }
 
 .button-2{
-  color: #1c918e;
-  border: 1px solid #1c918e;
+  color: white !important;
+  border: 1px solid #1c918e !important;
 }
 
 .button-2:hover{
-    background-color:#1c918e;
-    color: white;
+    background-color:#1c918e !important;
+    color: white !important;
 }
 </style>
